718901ada4c7fbaa6deed6a42334a0df8bf76a93
Andriy Iun <andriy.yun@gmail.com>
2020-03-02 13:09:12 +0100

    Update banner link field approach

--- src/Form/BannerForm.php
+++ src/Form/BannerForm.php
@@ -39,8 +39,6 @@ class BannerForm extends ContentEntityForm {
     /* @var \Drupal\os2web_banner\Entity\Banner $entity */
     $form = parent::buildForm($form, $form_state);
     $build_info = $form_state->getBuildInfo();
-    $form['#id'] = Html::getUniqueId($build_info['form_id']);
-    self::adjustForm($form, $form_state);
 
     if (!$this->entity->isNew()) {
       $form['new_revision'] = [
@@ -87,63 +85,5 @@ class BannerForm extends ContentEntityForm {
     }
     $form_state->setRedirect('entity.os2web_banner.canonical', ['os2web_banner' => $entity->id()]);
   }
-  /**
-   * Function that do adjust form for custom view.
-   *
-   * @param array $form
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   */
-  public static function adjustForm(array &$form, FormStateInterface $form_state) {
-    $wrapper_id = $form['#id'];
-    $form['type']['widget']['#ajax'] = [
-      'callback' => [static::class, 'ajaxCallback'],
-      'wrapper' => $wrapper_id,
-    ];
 
-    $form['field_os2web_banner_butt_link']['widget']['#ajax'] = [
-      'callback' => [static::class, 'ajaxCallback'],
-      'wrapper' => $wrapper_id,
-    ];
-
-    $entity = $form_state->getFormObject()->getEntity();
-    $field_os2web_banner_butt_link = $entity->field_os2web_banner_butt_link;
-    $link_type = NestedArray::getValue($form_state->getUserInput(), $form['field_os2web_banner_butt_link']['widget']['#parents']);
-    if (empty($link_type)) {
-      if (!$field_os2web_banner_butt_link->isEmpty()) {
-        $link_type = $field_os2web_banner_butt_link->first()->value;
-      }
-      else {
-        $link_type = '_none';
-      }
-    }
-    switch ($link_type) {
-      case '_none':
-        $form['field_os2web_banner_butt_link_i']['#access'] = FALSE;
-        $form['field_os2web_banner_butt_link_e']['#access'] = FALSE;
-        break;
-
-      case 'internal':
-        $form['field_os2web_banner_butt_link_e']['#access'] = FALSE;
-        break;
-
-      case 'external':
-        $form['field_os2web_banner_butt_link_i']['#access'] = FALSE;
-        break;
-    }
-  }
-
-  public static function ajaxCallback(array $form, FormStateInterface $form_state) {
-    $triggering_element = $form_state->getTriggeringElement();
-    $banner_form_parents = [];
-    foreach ($triggering_element['#field_parents'] as $key) {
-      $banner_form_parents[] = $key;
-      if (strpos($key, 'field_') === 0) {
-        $banner_form_parents[] = 'widget';
-      }
-    }
-    // @TODO Integration with Inline entity forms still works not well
-    // on edit mode.
-    $banner_form = NestedArray::getValue($form, $banner_form_parents);
-    return $banner_form;
-  }
 }
